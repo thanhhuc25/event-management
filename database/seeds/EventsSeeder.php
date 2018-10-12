185d0e27be85b2a68e69f1f2acbec112d1f277df
do <thanhdo53th@gmail.com>
2018-10-12 17:04:39 +0700

    fix dupplicate migrations

--- database/seeds/EventsSeeder.php
+++ database/seeds/EventsSeeder.php
@@ -16,17 +16,15 @@ class EventsSeeder extends Seeder
             [
                 "shop_name"=>"（有）チハラ金物店",
                 "open_date"=>"2018-09-08",
-                "open_datetime"=>"10:00～ 12:00, 13:00～14:00, 14:00～15:00",
-                "open_date2"=>"2018-10-08",
-                "open_datetime2"=>"10:00～ 12:00, 13:00～14:00, 14:00～15:00",
-                "phone"=> "0495-72-0014", "position"=>"埼玉県本庄市児玉町児玉１５１", "comment"=>"充実ラインナップのマキタ製品をご体感ください",
+                "phone"=> "0495-72-0014", "position"=>"埼玉県本庄市児玉町児玉１５１",
+                "comment"=>"充実ラインナップのマキタ製品をご体感ください",
                 "lat"=>36.18982500594721, "long"=>139.1293496, "province"=>11
             ],
             [
                 "shop_name"=>"中戸金物店",
                 "open_date"=>"2018-09-08",
-                "open_datetime"=>"10:00～ 12:00, 13:00～14:00, 14:00～15:00",
-                "phone"=> "0495-72-0014", "position"=>"埼玉県本庄市児玉町児玉１５１", "comment"=>"充実ラインナップのマキタ製品をご体感ください",
+                "phone"=> "0495-72-0014", "position"=>"埼玉県本庄市児玉町児玉１５１",
+                "comment"=>"充実ラインナップのマキタ製品をご体感ください",
                 "lat"=>36.18982500594721, "long"=>139.1293496, "province"=>11
             ]
 
@@ -37,9 +35,6 @@ class EventsSeeder extends Seeder
             $e->category_id = isset($event["category"]) ? $event["category"]: null;
             $e->shop_name = isset($event["shop_name"]) ? $event["shop_name"]: null;
             $e->open_date = isset($event["open_date"]) ? $event["open_date"]: null;
-            $e->open_date_time = isset($event["open_datetime"]) ? $event["open_datetime"]: null;
-            $e->open_date2 = isset($event["open_date2"]) ? $event["open_date2"]: null;
-            $e->open_date_time2 = isset($event["open_datetime2"]) ? $event["open_datetime2"]: null;
             $e->phone = isset($event["phone"]) ? $event["phone"]: null;
             $e->comment = isset($event["comment"]) ? $event["comment"]: null;
             $e->lat = isset($event["lat"]) ? $event["lat"]: null;
